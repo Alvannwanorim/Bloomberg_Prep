106b92c31c4e1134d93753cc90ef362836e5a609
alvannwanorim <alvannwanorim@gmail.com>
2023-03-18 22:59:44 +0100

    Two city scheduling

--- two_city_scheduling.py
+++ two_city_scheduling.py
@@ -5,8 +5,8 @@ class Solution:
     def twoCityScheduling(self, costs: List[List[int]]) -> int:
         costs = sorted(costs, key=lambda x: x[1]-x[0])
         res = 0
-        print(costs[:len(costs)//2])
-        print(costs[len(costs)//2:])
+        # print(costs[:len(costs)//2])
+        # print(costs[len(costs)//2:])
         for c in costs[len(costs)//2:]:
             res += c[0]
 
@@ -18,3 +18,5 @@ class Solution:
 
 sol = Solution()
 print(sol.twoCityScheduling([[10, 20], [30, 200], [400, 50], [30, 20]]))
+print(sol.twoCityScheduling([[259, 770], [448, 54], [
+      926, 667], [184, 139], [840, 118], [577, 469]]))
