784fe15ea770444694a2cce2927a50750c26f5cd
alvannwanorim <alvannwanorim@gmail.com>
2023-04-06 18:15:45 +0100

    Word Break II

---
+++ word_break__II.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        memo = {}
+
+        def dfs(s):
+            if s in memo:
+                return memo[s]
+
+            res = []
+            for x in wordDict:
+                if s.startswith(x):
+                    if s == x:
+                        res += [x]
+                    else:
+                        for y in dfs(s[len(x):]):
+                            res += [x + ' ' + y]
+            memo[s] = res
+            return res
+        return dfs(s)
+
+
+sol = Solution()
+print(sol.wordBreak('catsanddog', ["cat", "cats", "and", "sand", "dog"]))
