cf20bf600ba74279d1c19f50017b78821fa40000
alvannwanorim <alvannwanorim@gmail.com>
2023-04-08 19:00:01 +0100

    Binary tree vertical order traversal

---
+++ binary-tree_vertical_order_traversal.py
@@ -0,0 +1,37 @@
+from collections import defaultdict, deque
+import collections
+from typing import List, Optional
+
+
+class NodeTree:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.right = right
+        self.left = left
+
+
+class Solution:
+    def traversal(self, root: Optional[NodeTree]) -> List[List[int]]:
+        left, right = 0, 0
+        table = defaultdict(list)
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, col = queue.popleft()
+
+            if node is not None:
+                table[col].append(node.val)
+                right = max(right, col)
+                left = min(left, col)
+                queue.append((node.right, col + 1))
+                queue.append((node.left, col - 1))
+
+        return [table[x] for x in range(left, right + 1)]
+
+
+node1 = NodeTree(1)
+node2 = NodeTree(2, node1)
+node3 = NodeTree(3, None, node2)
+
+sol = Solution()
+print(sol.traversal(node3))
